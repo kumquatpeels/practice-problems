90c1db1e2a925a7ccbe1b44c7bf6f78d0a9eb10c
JiKwan Alexander Heo <jikster712@gmail.com>
2019-11-17 20:38:55 -0800

    drill02

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,50 @@
 
-function sumArray(){
-	
+function sumArray(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
 
-function fitWithinVal(){
+function fitWithinVal(arr,num) {
+  var sum = 0;
+  var answerArr = [];
 
+  for (var i = 0; i < arr.length; i++) {
+    var finalSum = sum += arr[i];
+
+    if (finalSum > num) {
+      return answerArr;
+    }
+
+    answerArr.push(arr[i]);
+  }
+  return answerArr;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(nameArr, nameLength) {
+//take length of each index of array and compare to nameLength
+//if short than push to answersArr, return answersArr
+var answersArr = [];
+  for (var i = 0; i < nameArr.length; i++) {
+    if (nameArr[i].length < nameLength) {
+      answersArr.push(nameArr[i]);
+    }
+  }
+  return answersArr;
 }
 
-function makeLabel(){
-
+function makeLabel(arr){
+// var greeting = arr.greeting;
+// var firstName = arr.givenName;
+// var lastName = arr.familyName;
+// var stNum = arr['home address'].streetNumber;
+// var stName = arr['home address'].streetName;
+// var city = arr['home address'].city;
+// var state = arr['home address'].state;
+// var zip = arr['home address'].zip;
+// var label = greeting + " " + firstName + " " + lastName + '\n' + stNum + " " + stName + '\n' + city + ", " + state + " " +  zip;
+// return label;
+return(arr.greeting + " " + arr.givenName + " " + arr.familyName + '\n' + arr['home address'].streetNumber + " " + arr['home address'].streetName + '\n' + arr['home address'].city + ", " + arr['home address'].state + " " + arr['home address'].zip)
 }
--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,15 +1,38 @@
 
 
-function countOccurences(){
-
+function countOccurences(arr, word){
+//compare word with each index of arr
+//store in answerArr
+  var answerArr = [];
+  for (i=0; i<arr.length; i++) {
+    if (word === arr[i]) {
+      answerArr.push(arr[i]);
+    }
+  }
+  return answerArr.length;
 }
 
-function wordLengths(){
-
+function wordLengths(wordArr){
+  var answersArr = [];
+  for (var i=0; i<wordArr.length; i++) {
+    answersArr.push(wordArr[i].length)
+  }
+  return answersArr;
 }
 
-function getMinMaxMean(){
-
+function getMinMaxMean(arr){
+  var min = Math.min(...arr);
+  var max = Math.max(...arr);
+  var sum = 0;
+  for (var i=0; i<arr.length; i++) {
+    sum += parseInt(arr[i],10);
+  }
+  var avg = sum / arr.length;
+  var answer = {};
+  answer.min = min;
+  answer.max = max;
+  answer.mean = avg;
+  return answer;
 }
 
 function findMode(){
